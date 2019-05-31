d14ae3cfbb0287f07c5ec13714e68f2416f8dc96
9ppf <48794672+9ppf@users.noreply.github.com>
2019-06-01 01:07:04 +0800

    首次提交项目数据

---
+++ public/js/yanzheng.js
@@ -0,0 +1,30 @@
+//   昵称验证
+function yanzheng(){
+    var $phone=${"#phone"};
+    var $js1=$("#jingshi");
+    $js1.css("opacity","1");
+    if($phone.value==""){
+        js1.innerHTML="您输入的昵称为空";
+        setTimeout(function(){jingshi1.innerHTML=""},1000);}
+    setTimeout(function(){$js1.css("opacity","0")},1000)
+    }
+    //    // 修改元素
+    //    if(uname.value==""){
+    //        js1.setAttribute("class","jingshi1");
+    //        js1.innerHTML="您输入的昵称为空";
+    //        setTimeout(function(){jingshi1.innerHTML=""},1000);
+    //    }else{
+    //        js1.setAttribute("class","jingshi2");
+    //        js1.innerHTML="正确";
+    //    }
+    //}
+    //uname.onfocus=function(){
+    //    if (!uname.value==""){js1.innerHTML="";}
+    //}
+
+
+
+
+//
+//var uname=document.getElementById("uname");
+//var upwd=document.getElementById("upwd");
